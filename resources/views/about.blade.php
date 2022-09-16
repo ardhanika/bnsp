2071807949800fd3591f9f58683e2e85df4e72c3
Ardhanika <dhani.scoo21@gmail.com>
2022-09-16 20:40:00 +0700

    first code

---
+++ resources/views/about.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="row bg-white overflow-hidden shadow-sm rounded px-8 mx-auto py-12" style="min-height: 427px;">
+            <div class="col-md-12">
+                <h3 style="font-size: 1.75rem!important">About</h3>
+            </div>
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-2">
+                        <img src="{{ asset('/img/avatar.jpg')}}"
+                            style="object-fit: cover;width: 150px;height: 150px;" class="rounded-circle"
+                            alt="The Promised Desire">
+                    </div>
+                    <div class="col-md-10">
+                        <p class="mt-4">Aplikasi ini dibuat oleh:</p>
+                        <p>Nama : Ardhanika Muji R.H.</p>
+                        <p>NIM : 1931710165</p>
+                        <p>Tanggal : 16 September 2022</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
